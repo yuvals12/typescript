648de47b61873ca28b815dcc23337951f7231e6f
yuvals12 <yuvalshauli12@gmail.com>
2023-01-12 21:10:41 +0200

    first task

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
 /*
+const getAverage1=(():void =>//לא הבנתי למה הפעולה מחיזרה לי את הקלט
+// ולא מבצעת את הלוגיקה ומחזירה ממוצע
+{ 
 let average:number = 0;
 let str: string=prompt("enter list of numbers") ?? "null";
-let arraynum:Array<number>=[];
-let arraystr:Array<string>=str.split(',');
-arraystr.map((value:string)=>
+let arraynum:Array<number>=str.split(',').map((value:string)=>
 {
-   arraynum.push( parseInt(value));
+return parseInt(value);
 }
 )
 arraynum.forEach((value:number) =>
 {
     average += value;
 })
-
-const completeave:number= average / arraynum.length;
-alert(arraystr);
-*/
+const completeave:number=average / arraynum.length;
+alert(arraynum);
+});
+getAverage1();
 /*
+
 const getAverage=((array:number[]) :number =>
    { let sum:number = 0;
     array.forEach((value) => {
@@ -39,10 +41,12 @@ console.log(getAmountOfPositive([-1.5, 6, 1.5]));
 */
 
 
-const sortList2=((array:number[]) :void =>
+const sortList=((array:number[]) :void =>
 { 
     array.sort(function(a,b){return a-b})
+    console.log(array);
 })
+console.log(sortList([-1.5, 6, 1.5]));
 
  
   
